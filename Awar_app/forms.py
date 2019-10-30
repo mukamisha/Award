8245ccd56df34918db4ef03018b57017d54bef57
mukamisha <mukamishajacky97@gmail.com>
2019-10-30 10:57:54 +0200

    excluding design field from the newpostform

--- Awar_app/forms.py
+++ Awar_app/forms.py
@@ -5,7 +5,7 @@ from .models import Image,Profile,Comment
 class NewPostForm(forms.ModelForm):
     class Meta:
         model = Image
-        exclude = ['profile','comments','user']
+        exclude = ['profile','comments','user','design']
 
 class ProfileForm(forms.ModelForm):
    class Meta:
